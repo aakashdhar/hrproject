8fd4c25fbc411d1d06595bb5cbb7f853b6e5fe58
Aakash Dhar <daxeffect007@gmail.com>
2018-12-19 18:01:03 +0530

    fixes for employee page

--- app/Http/Controllers/Employees/EmployeesController.php
+++ app/Http/Controllers/Employees/EmployeesController.php
@@ -15,10 +15,9 @@ class EmployeesController extends Controller
     {
         return view("employees.employee");
     }
-    //function is for updating employee 
+    //function is for updating employee
     public function updateEmployee($id,Request $request) {
         $user = User::find($id);
-        
         $user->user_name = $request->get('user_name');
         $user->user_first_name = $request->get('user_first_name');
         $user->user_last_name = $request->get('user_last_name');
@@ -26,15 +25,19 @@ class EmployeesController extends Controller
         $user->user_contact_no = $request->get('user_contact_no');
         $user->user_password = Hash::make($request->get('user_password'));
         $user->user_password_raw = $request->get('user_password');
-        
         $user->user_address = $request->get('user_address');
+        $user->joining_date = $request->get('joining_date');
+        $user->user_dob = $request->get('user_dob');
+        $user->user_hobbies = $request->get('user_hobbies');
+        $user->user_emergency_name = $request->get('user_emergency_name');
+        $user->user_emergency_contact = $request->get('user_emergency_contact');
         $res = $user->save();
         return redirect()->back();
     }
-    
+
     //this function is for delete employee
     public function deleteEmployee($id,Request $request) {
-        
+
         $user = User::find($id);
         $res = $user->delete();
         return redirect()->back();
--- resources/views/employees/employee.blade.php
+++ resources/views/employees/employee.blade.php
@@ -3,66 +3,64 @@
 @section('title', 'Employees')
 
 @section('content_header')
-    
+
 @stop
 @section('content')
-    
+
     <br>
     <div class="panel">
         <div class="panel-heading">
-        <br/>          
+        <br/>
         </div>
-        <?php 
+        <?php
         $count = 1;
-        $userdata = \Illuminate\Support\Facades\DB::select("SELECT * from users u,user_types ut where ut.user_type_id=u.user_type_id and ut.user_type!='Admin'") 
+        $userdata = \Illuminate\Support\Facades\DB::select("SELECT * from users u,user_types ut where ut.user_type_id=u.user_type_id and ut.user_type!='Admin'")
         ?>
         <div class="panel-body">
             <h2>Employees</h2>
             <table class="table text-center table-bordered table-hover">
                 <thead>
                     <th>NO.</th>
-                    <th>Username</th>
                     <th>Name</th>
                     <th>Email</th>
                     <th>Contact NO.</th>
                     <th>Address</th>
-                    <th>Password</th>
                     <th>User Type</th>
-                    <th></th>
+                    <th>Actions</th>
                 </thead>
                 <tbody>
                     @foreach($userdata as $val)
                     <tr>
                         <td>{{$count++}}</td>
-                        <td>{{$val->user_name or '-'}}</td>
-                        <td>{{$val->user_first_name or '-'}} {{$val->user_last_name or '-'}}</td>
+                        <td>{{ucfirst($val->user_first_name)}} {{ucfirst($val->user_last_name)}}</td>
                         <td>{{$val->user_email or '-'}}</td>
                         <td>{{$val->user_contact_no or '-'}}</td>
                         <td>{{$val->user_address or '-'}}</td>
-                        <td>{{$val->user_password_raw or '-'}}</td>
                         <td>{{$val->user_type or '-'}}</td>
                         <td>
-                            <form action="{{url('employees/sendmail')}}" method="post">
-                                {{ csrf_field() }}
-                                <input type="hidden" name="userid" value="{{$val->user_id}}">
-                                <input type="submit" class="btn" value="Send password">
-                            </form>
-                        </td>
-                        <td>
-                                <div style="display: inline-block">
-                                    <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteAdminModal_{{ $val->user_id }}">Delete</button>
-                                </div>
-                                <div style="display: inline-block">
-                                    <button class="btn btn-sm btn-primary" data-toggle="modal" data-target="#updateAdminModal_{{ $val->user_id }}">Update</button>
-                                </div>
+                          <div class="dropdown">
+                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                              Action
+                              <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                              <li><button class="btn btn-link" data-toggle="modal" data-target="#deleteAdminModal_{{ $val->user_id }}">Delete</button></li>
+                              <li><button class="btn btn-link" data-toggle="modal" data-target="#updateAdminModal_{{ $val->user_id }}">Update</button></li>
+                              <li><button class="btn btn-link" data-toggle="modal" data-target="#viewAdminModal_{{ $val->user_id }}">View Details</button></li>
+                              <li role="separator" class="divider"></li>
+                              <li>
+                                <form action="{{url('employees/sendmail')}}" method="post">
+                                  {{ csrf_field() }}
+                                  <input type="hidden" name="userid" value="{{$val->user_id}}">
+                                  <input type="submit" class="btn btn-link" value="Send password">
+                                </form>
+                              </li>
+                            </ul>
+                          </div>
                         </td>
                     </tr>
-                    
-                    
-            <!-- Update model  -->
                         <div id="updateAdminModal_{{ $val->user_id }}" class="modal fade" role="dialog">
                             <div class="modal-dialog modal-lg">
-
                                 <!-- Modal content-->
                                 <div class="modal-content">
                                     <form action="/employee/update/{{ $val->user_id }}" id="updateAdmin_{{ $val->user_id }}" method="POST">
@@ -96,20 +94,42 @@
                                                 </div>
                                                 <div class="col-md-4">
                                                     <label for="user_contact_no">Contact</label>
-                                                    <input type="number" name="user_contact_no" value="{{ $val->user_contact_no }}" id="user_contact_no" class="form-control" placeholder="Contact no">
+                                                    <input type="text" name="user_contact_no" value="{{ $val->user_contact_no }}" id="user_contact_no" class="form-control" placeholder="Contact no" maxlength="10">
                                                 </div>
-                                                <div class="col-md-4">
+                                                <div class="col-md-4"  style="display:none">
                                                     <label for="user_password">Password</label>
                                                     <input type="password" name="user_password" value="{{ $val->user_password_raw }}" id="user_password" class="form-control" placeholder="Password" required>
                                                 </div>
-
                                             </div>
                                             <div class="row" style="margin-top: 2%;">
                                                 <div class="col-md-4">
                                                     <label for="user_address">Address</label>
                                                     <input name="user_address" class="form-control" value="{{ $val->user_address }}" id="user_address" placeholder="Address">
-
                                                 </div>
+                                                <div class="col-md-4">
+                                                  <label for="user_address">Date of joining</label>
+                                                  <input type="date" name="joining_date" class="form-control" value="{{ $val->joining_date }}" id="joining_date" placeholder="Date of Joining">
+                                                </div>
+                                                <div class="col-md-4">
+                                                  <label for="user_address">Date of birth</label>
+                                                  <input type="date" name="user_dob" class="form-control" value="{{ $val->user_dob }}" id="user_dob" placeholder="Date of birth">
+                                                </div>
+                                            </div>
+                                            <div class="row" style="margin-top: 2%;">
+                                              <div class="col-md-4">
+                                                <label for="user_address">Emergency Contact</label>
+                                                <input name="user_emergency_name" class="form-control" value="{{ $val->user_emergency_name }}" id="user_emergency_name" placeholder="Emergency Contact">
+                                              </div>
+                                              <div class="col-md-4">
+                                                <label for="user_address">Emergency Contact number</label>
+                                                <input name="user_emergency_contact" class="form-control" value="{{ $val->user_emergency_contact }}" id="user_emergency_contact" placeholder="Emergency Contact Number" maxlength="10">
+                                              </div>
+                                            </div>
+                                            <div class="row" style="margin-top: 2%;">
+                                              <div class="col-md-12">
+                                                <label for="user_address">Hobbies</label>
+                                                <input name="user_hobbies" class="form-control" value="{{ $val->user_hobbies }}" id="user_hobbies" placeholder="Hobbies" required>
+                                              </div>
                                             </div>
                                         </div>
                                         <div class="modal-footer">
@@ -118,11 +138,79 @@
                                         </div>
                                     </form>
                                 </div>
-
                             </div>
                         </div>
+                        <div class="modal fade" id="viewAdminModal_{{ $val->user_id }}" tabindex="-1" role="dialog" >
+                            <div class="modal-dialog modal-lg">
+                               <div class="modal-content">
+                                 <div class="modal-header">
+                                   <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+                                   <h4 class="modal-title">{{ucfirst($val->user_first_name)}} {{ucfirst($val->user_last_name)}}</h4>
+                                 </div>
+                                 <div class="modal-body">
+                                   <div class="row">
+                                       <div class="col-md-4">
+                                           <label for="user_name">User name</label>
+                                           <input type="text" name="user_name" id="user_name" value="{{ $val->user_name }}" class="form-control" placeholder="First name" readonly>
+                                       </div>
+                                       <div class="col-md-4">
+                                           <label for="user_first_name">First name</label>
+                                           <input type="text" name="user_first_name" value="{{ $val->user_first_name }}" id="user_first_name" class="form-control" placeholder="First name" readonly>
+                                       </div>
+                                       <div class="col-md-4">
+                                           <label for="user_last_name">Last name</label>
+                                           <input type="text" name="user_last_name" value="{{ $val->user_last_name }}" id="user_last_name" class="form-control" placeholder="Last name" readonly>
+                                       </div>
 
-                        <!-- Delete model  -->
+                                   </div>
+                                   <div class="row" style="margin-top: 2%;">
+                                       <div class="col-md-4">
+                                           <label for="user_email">Email</label>
+                                           <input type="email" name="user_email" value="{{ $val->user_email }}" id="user_email" class="form-control" placeholder="Email" readonly>
+                                       </div>
+                                       <div class="col-md-4">
+                                           <label for="user_contact_no">Contact</label>
+                                           <input type="text" name="user_contact_no" value="{{ $val->user_contact_no }}" id="user_contact_no" class="form-control" placeholder="Contact no" readonly>
+                                       </div>
+                                   </div>
+                                   <div class="row" style="margin-top: 2%;">
+                                       <div class="col-md-4">
+                                           <label for="user_address">Address</label>
+                                           <input name="user_address" class="form-control" value="{{ $val->user_address }}" id="user_address" placeholder="Address" readonly>
+                                       </div>
+                                       <div class="col-md-4">
+                                         <label for="user_address">Date of joining</label>
+                                         <input type="text" name="joining_date" class="form-control" value="{{ date('d-M-Y',strtotime($val->joining_date)) }}" id="joining_date" placeholder="Date of Joining" readonly>
+                                       </div>
+                                       <div class="col-md-4">
+                                         <label for="user_address">Date of birth</label>
+                                         <input type="text" name="user_dob" class="form-control" value="{{ date('d-M-Y',strtotime($val->user_dob))}}" id="user_dob" placeholder="Date of birth" readonly>
+                                       </div>
+                                   </div>
+                                   <div class="row" style="margin-top: 2%;">
+                                     <div class="col-md-4">
+                                       <label for="user_address">Emergency Contact</label>
+                                       <input name="user_emergency_name" class="form-control" value="{{ $val->user_emergency_name }}" id="user_emergency_name" placeholder="Emergency Contact" readonly>
+                                     </div>
+                                     <div class="col-md-4">
+                                       <label for="user_address">Emergency Contact number</label>
+                                       <input name="user_emergency_contact" class="form-control" value="{{ $val->user_emergency_contact }}" id="user_emergency_contact" placeholder="Emergency Contact Number" readonly>
+                                     </div>
+                                   </div>
+                                  <div class="row" style="margin-top: 2%;">
+                                    <div class="col-md-12">
+                                      <label for="user_address">Hobbies</label>
+                                      <input name="user_hobbies" class="form-control" value="{{ $val->user_hobbies }}" id="user_hobbies" placeholder="Hobbies" readonly>
+                                    </div>
+                                  </div>
+                                 </div>
+                                 <div class="modal-footer">
+                                   <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                 </div>
+                               </div>
+                             </div>
+                        </div>
+                        <!-- Delete modal  -->
                         <div id="deleteAdminModal_{{ $val->user_id }}" class="modal fade" role="dialog">
                             <div class="modal-dialog">
 
@@ -148,7 +236,7 @@
                         </div>
                 @endforeach
                 </tbody>
-            </table>       
+            </table>
         </div>
     </div>
 
@@ -157,7 +245,7 @@
 
             <!-- Modal content-->
             <div class="modal-content">
-                
+
             </div>
 
         </div>
