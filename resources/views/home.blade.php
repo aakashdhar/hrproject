a1e3b9790b8f8381b965509e9a861621eb2eb67c
Aakash Dhar <daxeffect007@gmail.com>
2018-12-20 15:45:54 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,32 +21,34 @@
             <a href="/reminder" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-aqua">
-            <div class="inner">
-                <h3>{{ $employee_count }}</h3>
-                <p>Employees</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-person-stalker"></i>
-            </div>
-            <a href="/employees" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-aqua">
-            <div class="inner">
-                <h3>{{ $admin_count }}</h3>
-                <p>Admins</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-person"></i>
-            </div>
-            <a href="/admin" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
+    @if (\Auth::user()->user_type_id == '1')
+      <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+              <div class="inner">
+                  <h3>{{ $employee_count }}</h3>
+                  <p>Employees</p>
+              </div>
+              <div class="icon">
+                  <i class="ion ion-person-stalker"></i>
+              </div>
+              <a href="/employees" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+      </div>
+      <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+              <div class="inner">
+                  <h3>{{ $admin_count }}</h3>
+                  <p>Admins</p>
+              </div>
+              <div class="icon">
+                  <i class="ion ion-person"></i>
+              </div>
+              <a href="/admin" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+      </div>
+    @endif
     <div class="col-lg-3 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-aqua">
@@ -57,7 +59,7 @@
             <div class="icon">
                 <i class="ion ion-ios-email"></i>
             </div>
-            <a href="/reminder" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="/leave" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
 </div>
