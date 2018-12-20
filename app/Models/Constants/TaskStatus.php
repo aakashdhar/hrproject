c151ffe4dc27f2a90f827342b27e764cac8c9235
Harsh Shah <harsh.shah@siimteq.com>
2018-12-20 08:42:52 +0530

    fixes

---
+++ app/Models/Constants/TaskStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Constants;
+
+
+class TaskStatus
+{
+    const STARTED = 'Started';
+    const PAUSED = 'Paused';
+    const FINISHED = 'Finished';
+    const NOT_STARTED = 'Not Started Yet';
+}
