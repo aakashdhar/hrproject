3c4fb93c5fd4e8f390eae254bd147522cee67fa6
Harsh Shah <harsh.shah@siimteq.com>
2018-12-21 15:57:15 +0530

    added helper function

--- app/helpers.php
+++ app/helpers.php
@@ -5,3 +5,16 @@ if(!function_exists('ping')) {
         return 'pong';
     }
 }
+
+if (!function_exists('format_date')) {
+
+    function format_date($data_str, $format = 'd/m/Y')
+    {
+        if (empty($data_str)) {
+            return "";
+        }
+        $format = (empty($format) ? 'd/m/Y' : $format);
+        return date($format, strtotime($data_str));
+    }
+
+}
